e8e97fefc5ccc26eeb61ee45397820949cc7bb7e
yonatan <carri_2286@hotmail.com>
2025-12-22 23:40:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-full w-full bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-emerald-300">
+        Tailwind OK ✅
+      </h1>
+    </div>
+  );
+}
