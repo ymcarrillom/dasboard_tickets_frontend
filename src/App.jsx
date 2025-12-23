627e0e827b923e7516b2397ea24a82dbb4a8302c
yonatan <carri_2286@hotmail.com>
2025-12-23 05:43:37 -0500

    ap.jsx anda main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
+import DashboardPage from "./pages/DashboardPage.jsx";
+
 export default function App() {
-  return (
-    <div className="h-full w-full bg-slate-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-emerald-300">
-        Tailwind OK ✅
-      </h1>
-    </div>
-  );
+  return <DashboardPage />;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 10_000,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
